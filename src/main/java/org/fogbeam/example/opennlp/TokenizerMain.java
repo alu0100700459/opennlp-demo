9f87cb4fdc027a60bb926e63613e7c8acde9047b
alu0100700459 <alu0100700459@ull.edu.es>
2016-05-23 19:12:16 +0100

    Modification to read tokenizer inputs from files

--- src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
+++ src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
@@ -2,9 +2,14 @@
 package org.fogbeam.example.opennlp;
 
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.FileVisitOption;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
@@ -15,21 +20,41 @@ public class TokenizerMain
 {
 	public static void main( String[] args ) throws Exception
 	{
-		
-		// the provided model
-		// InputStream modelIn = new FileInputStream( "models/en-token.bin" );
+	  if (args.length == 0)
+	  {
+	    System.out.println("You have to specify a set of file names containing plain text.");
+	    return;
+	  }
 
-		
 		// the model we trained
 		InputStream modelIn = new FileInputStream( "models/en-token.model" );
 		
 		try
 		{
-			TokenizerModel model = new TokenizerModel( modelIn );
-		
+			TokenizerModel model = new TokenizerModel(modelIn);
 			Tokenizer tokenizer = new TokenizerME(model);
 			
-				/* note what happens with the "three depending on which model you use */
+			for (String fileName: args)
+			{
+			  ArrayList<String> fileTokens = new ArrayList<>();
+			  try (BufferedReader reader = new BufferedReader(new FileReader(fileName)))
+			  {
+  			  String line;
+  			  while ((line = reader.readLine()) != null)
+  			  {
+  			    String[] tokens = tokenizer.tokenize(line);
+  			    fileTokens.addAll(Arrays.asList(tokens));
+  			  }
+			  }
+			  finally
+			  {
+			    System.out.println("===== FILE: \"" + fileName + "\" =====");
+			    for (String token: fileTokens)
+            System.out.println(token);
+			  }
+			}
+			
+			/* note what happens with the "three depending on which model you use
 			String[] tokens = tokenizer.tokenize
 					(  "A ranger journeying with Oglethorpe, founder of the Georgia Colony, " 
 							+ " mentions \"three Mounts raised by the Indians over three of their Great Kings" 
@@ -39,7 +64,7 @@ public class TokenizerMain
 			{
 				System.out.println( token );
 			}
-			
+			*/
 		}
 		catch( IOException e )
 		{
