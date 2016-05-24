d8859eae7fc1eb1133e8e0fc8a0f82203e6ef4a2
alu0100700459 <alu0100700459@ull.edu.es>
2016-05-24 10:03:42 +0100

    Refactoring of TokenizerMain and addition of new tests

--- src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
+++ src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
@@ -9,10 +9,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import opennlp.tools.tokenize.Tokenizer;
 import opennlp.tools.tokenize.TokenizerME;
 import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.InvalidFormatException;
 
 /**
  * Tokenizer demo.
@@ -25,63 +27,71 @@ public class TokenizerMain
    * @param args The set of file names.
    * @throws Exception If some files are not found.
    */
-	public static void main( String[] args ) throws Exception
-	{
-	  if (args.length == 0)
-	  {
+	public static void main( String[] args ) throws Exception {
+	  if (args.length == 0) {
 	    System.out.println("You have to specify a set of file names containing plain text.");
 	    return;
 	  }
-
-		// the model we trained
-		InputStream modelIn = new FileInputStream( "models/en-token.model" );
-		
-		try
-		{
-			TokenizerModel model = new TokenizerModel(modelIn);
-			Tokenizer tokenizer = new TokenizerME(model);
-			
-			for (String fileName: args)
-			{
-			  ArrayList<String> fileTokens = new ArrayList<>();
-			  try (BufferedReader reader = new BufferedReader(new FileReader(fileName)))
-			  {
-  			  String line;
-  			  while ((line = reader.readLine()) != null)
-  			  {
-  			    String[] tokens = tokenizer.tokenize(line);
-  			    fileTokens.addAll(Arrays.asList(tokens));
-  			  }
-			  }
-			  catch (FileNotFoundException e)
-			  {
-			    throw e;
-			  }
-			  finally
-			  {
-			    System.out.println("===== FILE: \"" + fileName + "\" =====");
-			    for (String token: fileTokens)
-            System.out.println(token);
-			  }
-			}
-		}
-		catch( IOException e )
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			if( modelIn != null )
-			{
-				try
-				{
-					modelIn.close();
-				}
-				catch( IOException e )
-				{
-				}
-			}
-		}
+	  
+	  TokenizerMain tokenizer = new TokenizerMain();
+	  for (String fileName: args) {
+	    System.out.println("===== FILE: \"" + fileName + "\" =====");
+	    List<String> tokens = tokenizer.tokenizeFile(fileName);
+	    for (String token: tokens) {
+	      System.out.println(token);
+	    }
+	  }
+	  
 		System.out.println( "\n-----\ndone" );
 	}
+	
+  private Tokenizer tokenizer;
+  
+  /**
+   * Creates a new instance of the tokenizer.
+   * @throws InvalidFormatException If the model file used for training has an
+   * invalid format.
+   * @throws IOException If there's a problem reading the model file.
+   */
+  public TokenizerMain() throws InvalidFormatException, IOException {
+    InputStream modelIn = null;
+    
+    try {
+      modelIn = new FileInputStream("models/en-token.model");
+      TokenizerModel model = new TokenizerModel(modelIn);
+      tokenizer = new TokenizerME(model);
+    }
+    finally {
+      if (modelIn != null)
+        modelIn.close();
+    }
+  }
+  
+  /**
+   * Split a string by tokens.
+   * @param str The string to be split.
+   * @return An array of strings that represent the list of tokens.
+   */
+  public String[] tokenizeString(String str) {
+    return tokenizer.tokenize(str);
+  }
+  
+  /**
+   * Tokenizes the whole contents of a file.
+   * @param fileName The name of the file.
+   * @return The list of tokens in the file.
+   * @throws FileNotFoundException If the file specified doesn't exist.
+   * @throws IOException If there was a problem reading the file.
+   */
+  public List<String> tokenizeFile(String fileName) throws FileNotFoundException, IOException {
+    ArrayList<String> fileTokens = new ArrayList<>();
+    
+    try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+      String line;
+      while ((line = reader.readLine()) != null)
+        fileTokens.addAll(Arrays.asList(tokenizeString(line)));
+    }
+    
+    return fileTokens;
+  }
 }
--- src/test/java/org/fogbeam/example/opennlp/TestTokenizer.java
+++ src/test/java/org/fogbeam/example/opennlp/TestTokenizer.java
@@ -1,20 +1,22 @@
 package org.fogbeam.example.opennlp;
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
 import org.fogbeam.example.opennlp.TokenizerMain;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestTokenizer {
+  
+  private TokenizerMain tokenizer;
 
   @Before
   public void setUp() throws Exception {
-
-  }
-
-  @After
-  public void tearDown() throws Exception {
+    tokenizer = new TokenizerMain();
   }
 
   @Test
@@ -23,6 +25,35 @@ public class TestTokenizer {
       TokenizerMain.main(new String[]{"test/simpletext1.txt", "test/simpletext2.txt"});
     } catch (Exception e) {
       fail("This should not throw an Exception");
+    }
+  }
+  
+  @Test
+  public void testNonExistingFiles() {
+    try {
+      TokenizerMain.main(new String[]{"thisFileDoesNotExist"});
+      fail("This should have thrown an Exception");
+    } catch (Exception e) {}
+  }
+  
+  @Test
+  public void testStringTokenize() {
+    String str = "This is a simple string.";
+    String[] tokens = new String[]{"This", "is", "a", "simple", "string", "."};
+    String[] tokens2 = tokenizer.tokenizeString(str);
+    assertArrayEquals(tokens, tokens2);
+  }
+  
+  @Test
+  public void testFileTokenize() {
+    String[] tokens = new String[]{"This", "is", "the", "content", "of", "a", "simple", "test", "."};
+    try {
+      List<String> tokens2 = tokenizer.tokenizeFile("test/simpletext1.txt");
+      String[] tokens2Str = tokens2.toArray(new String[tokens2.size()]);
+      assertArrayEquals(tokens, tokens2Str);
+    } catch (IOException e) {
+      e.printStackTrace();
+      fail("This should not throw an exception");
     }
   }
 
